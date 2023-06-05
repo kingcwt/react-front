35f3d1f55edbd6bae20df56f1bd1c18cf6a50a2a
kingcwt <kingcwt@qq.com>
2023-06-05 17:07:36 +0800

    feat: 初始化-配置Dockerfile-nginx

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # react-front
 持续集成-前端demo
+
+
+- download taillWind
+
+```
+npm install -D tailwindcss postcss autoprefixer
+```
---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+type DataType = {
+  name: string;
+  email: string;
+  image: string;
+}
+
+function App() {
+  const [data, setData] = useState<DataType[]>([])
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('/api/list');
+      const jsonData = await response.json();
+      setData(jsonData)
+    })()
+  }, [])
+
+  return (
+    <>
+      <ul className="divide-y divide-gray-200 w-full">
+        {data.map((item) => (
+          <li key={item.email} className="py-4 flex">
+            <img className="h-10 w-10 rounded-full" src={item.image} alt="" />
+            <div className="ml-3">
+              <p className="text-sm font-medium text-gray-900">{item.name}</p>
+              <p className="text-sm text-gray-500">{item.email}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default App
