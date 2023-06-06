cfe99a4fde526b3877facb0a23d5bc8a023634b1
kingcwt <kingcwt@qq.com>
2023-06-06 22:38:48 +0800

    feat: update host

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server:{
-    // port:8080,
+    port:8080,
     proxy: {
+      host:'127.0.0.1',
       '/api': {
         target: 'http://121.41.62.126:3000/api',
         // target:'http://localhost:3000/api',
